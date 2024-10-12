51e4a7301ecb2df6cacd9b197d410de5c1d5fc51
Guaracy A. Lima <guaracyaraujolima@gmail.com>
2024-10-11 21:48:45 -0300

    base project

---
+++ src/main/java/br/com/zeroth/zthconfigserver/ZthConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.zeroth.zthconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZthConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZthConfigServerApplication.class, args);
+	}
+
+}
